66a6ec1897e355ce5ecafbf0003e32789d45759e
Dustin Michels <dustin7538@gmail.com>
2022-11-07 00:34:30 -0700

    start adding go backend

--- backend/server.go
+++ backend/server.go
@@ -3,15 +3,29 @@ package main
 import (
 	"net/http"
 
+	"github.com/dustinmichels/geo-hearo/api"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.GET("/places", func(c *gin.Context) {
+		places := api.GetPlaces()
+		c.JSON(http.StatusOK, places)
+	})
+
+	r.GET("/page", func(c *gin.Context) {
+		placeId := "T1pGvdXZ"
+		page := api.GetPage(placeId)
+		c.JSON(http.StatusOK, page)
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
