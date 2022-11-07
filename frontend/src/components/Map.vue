7fd436cd7b5390b8454b173af0701cdd826144f3
Dustin Michels <dustin7538@gmail.com>
2022-11-07 10:01:16 -0700

    working on map

---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    id="container"
+    ref="container"
+    style="position: relative; width: 500px; height: 300px"
+  ></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const container = document.getElementById('container')
+  //   const container = this.$refs.container
+  const map = new Datamap({
+    element: container,
+  })
+})
+</script>
