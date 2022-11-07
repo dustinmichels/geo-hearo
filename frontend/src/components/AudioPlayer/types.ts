55625d14f669ad88f1b732b9ebc154bed5b80775
Dustin Michels <dustin7538@gmail.com>
2022-11-06 19:42:02 -0700

    add initial audio player

---
+++ frontend/src/components/AudioPlayer/types.ts
@@ -0,0 +1,17 @@
+export interface AudioPlayerOption {
+  src: string //audio source
+  title?: string //audio title
+  coverImage?: string //cover image
+  coverRotate?: boolean //cover rotate when playing
+  progressBarColor?: string //progress bar color
+  indicatorColor?: string //indicator color
+}
+
+export const AudioPlayerOptionDefault: AudioPlayerOption = {
+  src: '',
+  title: '',
+  coverImage: '',
+  coverRotate: true,
+  progressBarColor: '#3C91F4',
+  indicatorColor: '#3C91F4',
+}
