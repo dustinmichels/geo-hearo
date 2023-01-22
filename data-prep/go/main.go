df5e01c5ef5e81e9af5c50b3fa5e98b1770f7329
Dustin Michels <dustin7538@gmail.com>
2023-01-22 13:44:35 -0700

    update go

--- data-prep/go/main.go
+++ data-prep/go/main.go
@@ -1,21 +1,68 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"sync"
 
 	"github.com/dustinmichels/geo-hearo/radiogarden"
 )
 
 func main() {
 
-	places, err := radiogarden.GetPlaces()
+	log.Println("GET Places")
+	placesResp, err := radiogarden.GetPlaces()
 	if err != nil {
 		panic(err)
 	}
 
-	p := places.Data.List[0]
+	// Save JSON
+	file, err := json.MarshalIndent(placesResp, "", " ")
+	if err != nil {
+		panic(err)
+	}
+	err = os.WriteFile("out/places.json", file, 0644)
+	if err != nil {
+		log.Fatalf("WriteFile: %s", err)
+	}
+	log.Println("WROTE out/places.json")
 
-	// fmt.Printf("Places: %+v\n", places)
-	fmt.Printf("Place: %+v\n", p)
+	// ---------- Get Places ----------
+
+	places := placesResp.Data.List[:10]
+
+	var wg sync.WaitGroup
+	wg.Add(len(places))
+
+	for i := range places {
+		go func(i int) {
+			defer wg.Done()
+
+			place := places[i]
+			log.Printf("GET [%d] - %s\n", i, place.Title)
+
+			// GET Request
+			resp, err := radiogarden.GetPlaceChannels(place.ID)
+			if err != nil {
+				log.Printf("ERROR [%d] - %s: %s\n", i, place.Title, err)
+				return
+			}
+
+			// Save JSON
+			file, err := json.MarshalIndent(resp, "", " ")
+			if err != nil {
+				panic(err)
+			}
+			err = os.WriteFile(fmt.Sprintf("out/place_%d.json", i), file, 0644)
+			if err != nil {
+				log.Fatalf("WriteFile: %s", err)
+			}
+			log.Printf("WROTE out/place_%d.json", i)
+		}(i)
+	}
+
+	wg.Wait()
 
 }
--- data-prep/go/radiogarden/get_places.go
+++ data-prep/go/radiogarden/get_places.go
@@ -7,24 +7,26 @@ import (
 	"net/http"
 )
 
-type Places struct {
+type PlacesResp struct {
 	APIVersion int    `json:"apiVersion"`
 	Version    string `json:"version"`
 	Data       struct {
-		List []struct {
-			Size    int       `json:"size"`
-			ID      string    `json:"id"`
-			Geo     []float64 `json:"geo"`
-			URL     string    `json:"url"`
-			Boost   bool      `json:"boost"`
-			Title   string    `json:"title"`
-			Country string    `json:"country"`
-		} `json:"list"`
-		Version string `json:"version"`
+		List    []PlacesResp_Place `json:"list"`
+		Version string             `json:"version"`
 	} `json:"data"`
 }
 
-func GetPlaces() (*Places, error) {
+type PlacesResp_Place struct {
+	Size    int       `json:"size"`
+	ID      string    `json:"id"`
+	Geo     []float64 `json:"geo"`
+	URL     string    `json:"url"`
+	Boost   bool      `json:"boost"`
+	Title   string    `json:"title"`
+	Country string    `json:"country"`
+}
+
+func GetPlaces() (*PlacesResp, error) {
 
 	url := "https://radio.garden/api/ara/content/places"
 	resp, err := http.Get(url)
@@ -42,7 +44,7 @@ func GetPlaces() (*Places, error) {
 		return nil, fmt.Errorf("GetPlaces: API error: %s", string(body))
 	}
 
-	var places Places
+	var places PlacesResp
 	if err := json.Unmarshal(body, &places); err != nil {
 		return nil, fmt.Errorf("GetPlaces: unmarshal error: %w", err)
 	}
