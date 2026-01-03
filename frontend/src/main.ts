5aee0150a9bc8d8e2c0f9d0f3a06d306c962611b
Dustin Michels <dustin7538@gmail.com>
2026-01-03 12:06:17 +0000

    add plausible

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,5 +3,10 @@ import App from './App.vue'
 import 'vant/lib/index.css'
 import './assets/styles/style.css'
 import router from './router'
+import { init } from '@plausible-analytics/tracker'
+
+init({
+  domain: 'geohearo.com',
+})
 
 createApp(App).use(router).mount('#app')
